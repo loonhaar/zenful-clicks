30fde1bed24de1592c225d29abed7d45f9efda48
cipher0ne <199516992+loonhaar@users.noreply.github.com>
2026-04-30 16:49:55 +0200

    Starting point

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
-fn main() {
-    println!("Hello, world!");
+use color_eyre::eyre::{Ok, Result};
+use ratatui::{
+	DefaultTerminal,
+	Frame,
+	crossterm::event::{self, Event}, widgets::{Paragraph, Widget}
+};
+
+fn main() -> Result<()> {
+	color_eyre::install()?;
+
+	let terminal = ratatui::init();
+	let result = run(terminal);
+
+	ratatui::restore();
+	result
+}
+
+fn run(mut terminal: DefaultTerminal) -> Result<()> {
+	loop {
+		// Rendering
+		terminal.draw(render)?;
+
+		// Input handling
+		if let Event::Key(key) = event::read()? {
+			match key.code {
+				event::KeyCode::Char('q') => break,
+				event::KeyCode::Esc 	  => break,
+				_ => {}
+			}
+		}
+	}
+
+	Ok(())
+}
+
+fn render(frame: &mut Frame) {
+	Paragraph::new("Hi! :3").render(frame.area(), frame.buffer_mut());
 }
